8231b0b266f3de2dbfffd7d3367c18532cb05bd4
skriewel <s.kriewel@gmail.com>
2026-08-31 12:12:39 +0200

    feat(decks): add cube deck kind

--- assets/react/src/pages/decks/queries.ts
+++ assets/react/src/pages/decks/queries.ts
@@ -11,6 +11,7 @@ export const DecksDocument = graphql(`
         node {
           id
           name
+          kind
           format
           status
           playCount
@@ -47,6 +48,7 @@ export const RandomDeckDocument = graphql(`
     randomDeck(excludeId: $excludeId) {
       id
       name
+      kind
       format
       status
       coverImageUrl
@@ -78,6 +80,7 @@ export const CreateDeckDocument = graphql(`
       deck {
         id
         name
+        kind
         format
         status
         primer
@@ -111,7 +114,7 @@ export const UpdateDeckDocument = graphql(`
       deck {
         id
         name
-        format
+        kind\n        format
         status
         playCount
         skipCount
@@ -218,7 +221,7 @@ export const DeckDocument = graphql(`
     deck(id: $id) {
       id
       name
-      format
+      kind\n      format
       status
       primer
       aiAnalysis
