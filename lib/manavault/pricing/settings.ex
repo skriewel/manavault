c03c01eaab52d5fade3326ff5ecd0bb2774b1ac5
skriewel <s.kriewel@gmail.com>
2026-09-01 16:35:35 +0200

    feat(pricing): store ECB exchange rate

--- lib/manavault/pricing/settings.ex
+++ lib/manavault/pricing/settings.ex
@@ -5,11 +5,13 @@ defmodule Manavault.Pricing.Settings do
 
   import Ecto.Changeset
 
-  @sources ~w(scryfall tcgplayer cardkingdom manapool)
+  @sources ~w(scryfall cardmarket tcgplayer cardkingdom manapool)
 
   @primary_key {:id, :integer, autogenerate: false}
   schema "pricing_settings" do
     field :source, :string, default: "scryfall"
+    field :usd_per_eur, :float
+    field :fx_rate_date, :date
 
     timestamps(type: :utc_datetime)
   end
@@ -21,5 +23,12 @@ defmodule Manavault.Pricing.Settings do
     |> cast(attrs, [:source])
     |> validate_required([:source])
     |> validate_inclusion(:source, @sources)
+  end
+
+  def exchange_rate_changeset(settings, attrs) do
+    settings
+    |> cast(attrs, [:usd_per_eur, :fx_rate_date])
+    |> validate_required([:usd_per_eur, :fx_rate_date])
+    |> validate_number(:usd_per_eur, greater_than: 0)
   end
 end
