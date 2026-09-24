735939ef597eb5cf6ef2a73f9a81e038e8e1da5c
skriewel <s.kriewel@gmail.com>
2026-09-24 21:14:17 +0200

    chore(merge): align overlapping tests with upstream

--- test/manavault_web/controllers/app_controller_test.exs
+++ test/manavault_web/controllers/app_controller_test.exs
@@ -72,7 +72,7 @@ defmodule ManavaultWeb.AppControllerTest do
     assert response =~ ~s|property="og:title" content="Lotus Lessons · ManaVault"|
 
     assert response =~
-             ~s|property="og:description" content="Commander deck, 100 cards, Legal, €256."|
+             ~s|property="og:description" content="Commander deck, 100 cards, Legal, $256.76."|
 
     refute response =~ "unique"
 
@@ -135,7 +135,7 @@ defmodule ManavaultWeb.AppControllerTest do
     assert response =~ "100 cards"
     refute response =~ "unique"
     assert response =~ "Legal"
-    assert response =~ "€256"
+    assert response =~ "$256.76"
     assert response =~ "data:image/svg+xml"
     assert response =~ ~s(<clipPath id="cardClip">)
     assert response =~ ~s|clip-path="url(#cardClip)"|
@@ -238,7 +238,7 @@ defmodule ManavaultWeb.AppControllerTest do
           "oracle_id" => "oracle-preview-commander",
           "name" => "Lotus Tutor",
           "image_uris" => %{"art_crop" => preview_cover_data_uri()},
-          "prices" => %{"eur" => "256.76"}
+          "prices" => %{"usd" => "256.76"}
         }),
         Map.merge(CatalogTestSupport.legal_plains(), %{
           "id" => "scryfall-preview-plains",
